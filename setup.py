592c00e8a40f0f499606c32658966d61b93b762e
Alan Matzumiya <alan.matzumiya@outlook.es>
2020-09-09 10:17:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
